1863bc78749847e306f0fbc280e3bd22f3902e1b
Park YoungJin <dkr979697@gmail.com>
2019-09-05 19:39:29 +0900

    completed design SplashActivity

--- presentation/src/main/java/com/tistory/dsmparkyoungjin/studentable/presentation/ui/MainActivity.java
+++ presentation/src/main/java/com/tistory/dsmparkyoungjin/studentable/presentation/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.tistory.dsmparkyoungjin.studentable;
+package com.tistory.dsmparkyoungjin.studentable.presentation.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.tistory.dsmparkyoungjin.studentable.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ presentation/src/main/java/com/tistory/dsmparkyoungjin/studentable/presentation/ui/splash/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.tistory.dsmparkyoungjin.studentable.presentation.ui.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tistory.dsmparkyoungjin.studentable.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
